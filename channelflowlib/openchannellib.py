4ed6ac5877cdbe44ecfb40c3b6e622bf2ffc510e
syncster31 <alexius.academia@gmail.com>
2017-10-30 00:08:28 +0800

    Added .travis.yml

--- channelflowlib/openchannellib.py
+++ channelflowlib/openchannellib.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License         #
 # along with this program.  If not, see <http://www.gnu.org/licenses/>      #
 # --------------------------------------------------------------------------#
-# File:		    openchannel.py                                              #
+# File:		    openchannellib.py                                              #
 # Author:		Alexius Academia                                            #
 # Email:		alexius.academia@gmail.com                                  #
 # --------------------------------------------------------------------------#

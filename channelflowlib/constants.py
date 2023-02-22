f3011db76746d7718ce864fc70974fd6ecd7a179
Alexius Academia <alexius.academia@gmail.com>
2023-02-22 08:05:10 +0800

    Using constant for gravitational acceleration

---
+++ channelflowlib/constants.py
@@ -0,0 +1,1 @@
+GRAVITY_G = 9.81 # m/s^2 - Acceleration due to gravity
--- channelflowlib/critical_flow.py
+++ channelflowlib/critical_flow.py
@@ -1,5 +1,7 @@
 import math
 
+from .constants import GRAVITY_G
+
 
 def solve_critical_flow_rectangular(flow_area: float,
                                     base_width: float,
@@ -7,14 +9,14 @@ def solve_critical_flow_rectangular(flow_area: float,
                                     discharge: float,
                                     manning_roughness: float):
     hydraulic_depth = flow_area / base_width
-    froude_number = average_velocity / math.sqrt(9.81 * hydraulic_depth)
+    froude_number = average_velocity / math.sqrt(GRAVITY_G * hydraulic_depth)
     discharge_intensity = discharge / base_width
-    critical_depth = math.pow(math.pow(discharge_intensity, 2.0) / 9.81, (1.0 / 3.0))
+    critical_depth = math.pow(math.pow(discharge_intensity, 2.0) / GRAVITY_G, (1.0 / 3.0))
     critical_top_width = base_width
     critical_wetted_perimeter = base_width + (2 * critical_depth)
     critical_flow_area = base_width * critical_depth
     critical_hydraulic_radius = critical_flow_area / critical_wetted_perimeter
-    critical_slope = 9.81 * math.pow(manning_roughness, 2.0) * \
+    critical_slope = GRAVITY_G * math.pow(manning_roughness, 2.0) * \
                      (critical_wetted_perimeter / critical_top_width) / \
                      math.pow(1, 2.0) * math.pow(critical_hydraulic_radius, (1.0/3.0))
 
